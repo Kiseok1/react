436a7869abc8aec2417da5ac4befbf4b7a798d34
Kiseok1 <s72111374@gmail.com>
2024-03-27 22:26:33 +0900

    modified:   begin-react/src/App.js

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,8 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function Article(props){
+function Create(props){
+  return <article>
+  <h2>Create</h2>
+  <form onSubmit={event => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    props.onCreate(title,body);
+  }}>
+    <p><input type="text" name='title' placeholder='title'></input></p>
+    <p><textarea name='body' placeholder='body' /></p>
+    <p><input type="submit" value='Create'></input></p>
+  </form>
+</article>
+}
 
+function Article(props){
   return <article>
     <h2>{props.title}</h2>
     {props.body}
@@ -41,12 +56,15 @@ function App() {
   // const setMode = _mode[1];
   const [mode,setMode] = useState('WELCOME');
   const [id,setId] = useState(null);
-  const topics = [
+  const [nextId,setNextId] = useState(4);
+  const [topics,setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ])
+    
   let content = null;
+  let contextControl = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
   } else if (mode === 'READ'){
@@ -59,6 +77,19 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+    contextControl = <>
+    </>
+
+  } else if (mode === 'CREATE'){
+    content = <Create onCreate={(_title,_body) => {
+      const newTopic = {id : nextId, title : _title, body : _body}
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
   }
   return (
     <div>
@@ -70,6 +101,13 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <ul>
+        <li><a href="/create" onClick={event => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
